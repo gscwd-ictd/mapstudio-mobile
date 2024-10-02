cbd01515f17e731b54e3b47c81828eedaca770e3
IANSR_GSCWD <ianspencer0904@gmail.com>
2024-10-02 16:49:50 +0800

    partial color scheme implementation

---
+++ lib/common/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.blue;
+  static const Color accentColor = Colors.orange;
+  static const Color mainColor = Color.fromARGB(255, 37, 99, 235);
+}
