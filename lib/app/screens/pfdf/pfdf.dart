690e92fbf905a047701c53fce1ff92e4104e2b11
IANSR_GSCWD <ianspencer0904@gmail.com>
2025-02-11 10:15:29 +0800

    pfdf tooltip buttons

--- lib/app/screens/pfdf/pfdf.dart
+++ lib/app/screens/pfdf/pfdf.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:mapstudio/app/widgets/modals/undo_pfdf_modal.dart';
+import 'package:mapstudio/app/widgets/modals/update_pfdf_modal.dart';
 import 'package:mapstudio/common/enums/saco_status_enum.dart';
 import 'package:mapstudio/data/models/pfdf_model.dart';
 import 'package:sizer/sizer.dart';
@@ -63,7 +66,7 @@ class _PfdfState extends State<Pfdf> {
       body: Column(
         children: [
           Expanded(
-              flex: 1,
+              flex: !isEditing ? 2 : 3,
               child: Container(
                 decoration: const BoxDecoration(
                   border: Border(
@@ -77,40 +80,64 @@ class _PfdfState extends State<Pfdf> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       !isEditing
-                          ? GestureDetector(
+                          ? fixtureButton(
+                              btn: 'Edit',
+                              clrIcon: Colors.grey[500]!,
+                              clrText: Colors.grey[500]!,
+                              icon: FontAwesomeIcons.penToSquare,
                               onTap: () {
                                 setState(() {
                                   isEditing = true;
                                 });
-                              },
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.penToSquare,
-                                    color: Colors.grey[500],
-                                  ),
-                                  Padding(
-                                    padding:
-                                        const EdgeInsets.fromLTRB(5, 3, 0, 0),
-                                    child: Text(
-                                      'Edit',
-                                      style: TextStyle(
-                                          color: Colors.grey[500],
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
+                              })
+                          : Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Row(
+                                  children: [
+                                    fixtureButton(
+                                        btn: 'Undo',
+                                        clrIcon: Colors.grey[500]!,
+                                        clrText: Colors.grey[500]!,
+                                        icon: MingCute.back_fill,
+                                        onTap: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) =>
+                                                  const UndoPfdfModal());
+                                        }),
+                                    SizedBox(
+                                      width: 2.w,
                                     ),
-                                  )
-                                ],
-                              ))
-                          : Text('test'),
+                                    fixtureButton(
+                                        btn: 'Update',
+                                        clrIcon: Colors.blue[500]!,
+                                        clrText: Colors.blue[500]!,
+                                        icon: MingCute.save_2_line,
+                                        onTap: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) =>
+                                                  const UpdatePFDFModal());
+                                        }),
+                                  ],
+                                ),
+                                DefaultButton(
+                                  onPressed: () {},
+                                  buttonText: 'Add Plumbing Fixture',
+                                  txtSize: 16.sp,
+                                  buttonWidth: 2.w,
+                                  radius: BorderRadius.circular(8),
+                                )
+                              ],
+                            ),
                     ],
                   ),
                 ),
               )),
           Expanded(
-            flex: 15,
+            flex: 20,
             child: ListView.builder(
                 padding: const EdgeInsets.fromLTRB(0, 4, 0, 20),
                 itemCount: widget.pfdf.length,
@@ -124,7 +151,7 @@ class _PfdfState extends State<Pfdf> {
                 }),
           ),
           Expanded(
-              flex: 2,
+              flex: 3,
               child: Padding(
                 padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
                 child: SizedBox(
@@ -139,5 +166,38 @@ class _PfdfState extends State<Pfdf> {
         ],
       ),
     );
+  }
+
+  GestureDetector fixtureButton(
+      {required String btn,
+      required IconData icon,
+      required Color clrIcon,
+      required clrText,
+      required Function onTap}) {
+    return GestureDetector(
+        onTap: () {
+          onTap();
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FaIcon(icon, color: clrIcon
+                // FontAwesomeIcons.penToSquare,
+                // color: Colors.grey[500],
+                ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 3, 0, 0),
+              child: Text(
+                btn,
+                style: TextStyle(
+                    color: clrText,
+                    // color: Colors.grey[500],
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ));
   }
 }
---
+++ lib/app/widgets/modals/undo_pfdf_modal.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:mapstudio/app/widgets/buttons/default_button.dart';
+import 'package:sizer/sizer.dart';
+
+class UndoPfdfModal extends StatefulWidget {
+  const UndoPfdfModal({super.key});
+
+  @override
+  State<UndoPfdfModal> createState() => _UndoPfdfModalState();
+}
+
+class _UndoPfdfModalState extends State<UndoPfdfModal> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      titlePadding: const EdgeInsets.symmetric(horizontal: 0, vertical: 16),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Text(
+              'Cancel the changes?',
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.sp,
+              ),
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+            color: Colors.red,
+          ),
+        ],
+      ),
+      content: const Text(
+        'Are you sure you want to cancel the changes you have made to the applicant’s Plumbing Fixtures Declaration Form (PFDF)?',
+        style: TextStyle(fontSize: 16),
+      ),
+      actionsPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            DefaultButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                buttonText: 'No',
+                buttonWidth: 10.w,
+                radius: BorderRadius.circular(8),
+                btnColor: Colors.white,
+                txtColor: Colors.grey,
+                txtSize: 16.sp),
+            DefaultButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                buttonText: 'Yes',
+                buttonWidth: 10.w,
+                radius: BorderRadius.circular(8),
+                btnColor: Colors.red,
+                txtColor: Colors.white,
+                txtSize: 16.sp)
+          ],
+        )
+      ],
+    );
+  }
+}
